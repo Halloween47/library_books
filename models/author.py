321442e40d988d196b0c8660851f942f69579322
Thomas Leconte <thomasl.dw20@gmail.com>
2025-05-19 20:50:07 +0200

    Vue kanban

--- models/author.py
+++ models/author.py
@@ -4,6 +4,6 @@ class LibraryAuthor(models.Model):
     _name = 'library.author'
     _description = 'Author'
 
-    name = fields.Char(string="Author Name", required=True)
-    birth_date = fields.Date(string="Birth Date")
-    nationality = fields.Char(string="Nationality")
+    name = fields.Char(string="Nom Auteur", required=True)
+    birth_date = fields.Date(string="Date de Naissance")
+    nationality = fields.Char(string="Nationnalité")
